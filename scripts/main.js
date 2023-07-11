1d57ad922e9fb4e4a842675f8b9bba691d99b1d8
timlong1994 <67029684+timlong1994@users.noreply.github.com>
2023-07-11 08:31:35 +0800

    Refactor main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,60 +1,26 @@
-//const myHeading = document.querySelector("h1");
-//myHeading.textContent = "Hello world!";
-
-
 const API_KEY = "1c5a98bf8b524cf5bb3185319230507";
+const API_URL = "http://api.weatherapi.com/v1/current.json?";
 
-//testing stuff please ignore
-
-const newCity = {
-    firstName: "",
-    cityName: document.getElementById("search_bar")
-} 
-
-let cityName = document.getElementById("sampleform");
-
-
-newCity.firstName = document.getElementById("search_bar").onsubmit;
-
-const sampleform = document.getElementById("search_bar");
-
-
-async function printSearchResult(weatherReport) {
+async function displayWeather(weatherReport) {
     document.getElementById("weather").innerHTML = weatherReport;
 } 
 
-function apiURL(city) {
-    url = "http://api.weatherapi.com/v1/current.json?key=1c5a98bf8b524cf5bb3185319230507&q=";
-    return url.concat(city);
+function concatURL(city) {
+    return API_URL.concat("key=", API_KEY, "&q=", city);
 }
 
 async function getweather(url) {
     const response = await fetch(url);
-    var data = await response.json();
+    let weatherReport = await response.json();
     
-    printSearchResult(data.current.condition.text)
+    displayWeather(weatherReport.current.condition.text)
 }
-
-async function getCityName() {
-    console.log(document.getElementById("submit"));
-    newCity.firstName = document.getElementById("search_bar").onsubmit;
-    url = apiURL(newCity.firstName);
-
-    const response = await fetch(url);
-    var data = await response.json();
-    const sampleform = await document.getElementById("fsearch_bar");
-    console.log(data);
-    printSearchResult(data.current.condition.text);
-    console.log(sampleform.value);
-}
-
 
 async function getWeather(event) {
-    let weather = city.q.value;
-    let searchurl = await apiURL(weather);
-    getweather(searchurl);
+    let location = locationName.q.value;
+    let url = await concatURL(location);
+    getweather(url);
     event.preventDefault();
 }
-const city = document.getElementById("sampleform")
-const comeon = document.getElementById("city");
-city.addEventListener("submit", getWeather);
+const locationName = document.getElementById("get_location");
+locationName.addEventListener("submit", getWeather);

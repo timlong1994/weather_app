d3f74d81bc24385af874dde4be17e43a5c5c1340
timlong1994 <67029684+timlong1994@users.noreply.github.com>
2023-07-12 01:04:02 +0800

    Add error handling

--- scripts/main.js
+++ scripts/main.js
@@ -1,32 +1,57 @@
 const API_KEY = "1c5a98bf8b524cf5bb3185319230507";
 const API_URL = "http://api.weatherapi.com/v1/current.json?";
+
 const locationName = document.getElementById("get_location");
 
 
-function displayWeather(weather) {
-    document.getElementById("weather").innerHTML = weather;
-} 
+function displayResult(weather) {
+    document.getElementById("search_result").innerHTML = `It's ${weather} in ${locationName.q.value} right now`;
+}
 
-function displayCity() {
-    document.getElementById("city").innerHTML = locationName.q.value;
+function displayError(error_message) {
+    document.getElementById("search_result").innerHTML = error_message;
 }
 
 function concatURL() {
-    return (API_URL + "key=" + API_KEY + "&q=" + locationName.q.value);
+    return (`${API_URL}key=${API_KEY}&q=${locationName.q.value}`);
 }
 
 async function getWeather(url) {
     const response = await fetch(url);
     let weatherReport = await response.json()
 
-    return weatherReport.current.condition.text;
+    try {
+        return weatherReport.current.condition.text;
+    } catch (undefined) {
+        return weatherReport.error.code;
+    }
 }
 
 async function returnSearchResult() {
-    //let location = locationName.q.value;
     let url = concatURL();
     let weather = await getWeather(url);
 
-    displayWeather(weather);
-    displayCity();
+    switch(weather) {
+        case 1003:
+            displayError("Search bar must have input");
+            break;
+        case 1006:
+            displayError("Please enter a valid location");
+            break;
+        case 9001:
+            displayError("Too many locations: please reduce the number of locations");
+            break;
+        case 1002:
+        case 1005:
+        case 2006:
+        case 2007:
+        case 2008:
+        case 2009:
+        case 9000:
+        case 9999:
+            displayError("We fucked up and are trying our best to fix it, please try again later");
+            break;
+        default:
+            displayResult(weather);
+    }
 }

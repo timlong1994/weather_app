a8bff75003d8f3d3ce468d8be789d121590aab53
timlong1994 <67029684+timlong1994@users.noreply.github.com>
2023-07-11 20:23:01 +0800

    replace eventListener in main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,27 +1,32 @@
 const API_KEY = "1c5a98bf8b524cf5bb3185319230507";
 const API_URL = "http://api.weatherapi.com/v1/current.json?";
 const locationName = document.getElementById("get_location");
-locationName.addEventListener("submit", returnSearchResult);
 
-function displayWeather(weatherReport) {
-    document.getElementById("weather").innerHTML = weatherReport;
+
+function displayWeather(weather) {
+    document.getElementById("weather").innerHTML = weather;
 } 
 
-function concatURL(city) {
-    return (API_URL + "key=" + API_KEY + "&q=" + city);
+function displayCity() {
+    document.getElementById("city").innerHTML = locationName.q.value;
+}
+
+function concatURL() {
+    return (API_URL + "key=" + API_KEY + "&q=" + locationName.q.value);
 }
 
 async function getWeather(url) {
     const response = await fetch(url);
-    let weatherReport = await response.json();
-    
-    displayWeather(weatherReport.current.condition.text)
+    let weatherReport = await response.json()
+
+    return weatherReport.current.condition.text;
 }
 
-async function returnSearchResult(event) {
-    let location = locationName.q.value;
-    let url = await concatURL(location);
+async function returnSearchResult() {
+    //let location = locationName.q.value;
+    let url = concatURL();
+    let weather = await getWeather(url);
 
-    getWeather(url);
-    event.preventDefault();
+    displayWeather(weather);
+    displayCity();
 }
